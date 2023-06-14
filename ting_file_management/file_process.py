b1fc387ba225e08fe596baec08c60af6dc067e56
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-14 17:55:31 -0300

    enqueue dictionary

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,40 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    for element in range(len(instance)):
+        if path_file == instance.search(element)['nome_do_arquivo']:
+            return None
+
+    data = txt_importer(path_file)
+    new_dictionary = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+
+    instance.enqueue(new_dictionary)
+    sys.stdout.write(str(new_dictionary))
+
+# se arquivo já existe deve ser ignorado
+# senão, mostra dados via stdout
+# esta function não retorna nada
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance.is_empty():
+        return instance.queue.pop(0)
+    return None
+    """dequeue"""
+# remove um dicionario que foi inserido na fila
+# deve exibir mensagem correta via stdout
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position.is_empty():
+        return None
+    return instance[position]
+    """search"""
+# deve exibir mensagem correta via stdout
