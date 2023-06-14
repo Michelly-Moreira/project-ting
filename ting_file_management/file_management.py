40ee41249479233ff78710b21419fbb203f3b69f
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-14 14:49:01 -0300

    abrindo arquivo para leitura

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    with open (path_file, "r") as file:
+        content = file.read()
+        print (content)
+
+
+    # Caso o arquivo TXT não exista aparece a msg Arquivo {path_file} não encontrado na stderr, onde path_file é o caminho do arquivo
+    # caso a extensão do arquivo seja diferente de .txt Formato inválido na stderr
+    # A função deve retornar uma lista contendo as linhas do arquivo
