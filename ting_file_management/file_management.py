de6240089a028fdbaf8a2237f54baf58ecb63196
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-14 15:37:32 -0300

    condições realizadas

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,11 +1,11 @@
 import sys
 
+
 def txt_importer(path_file):
-    with open (path_file, "r") as file:
-        content = file.read()
-        print (content)
-
-
-    # Caso o arquivo TXT não exista aparece a msg Arquivo {path_file} não encontrado na stderr, onde path_file é o caminho do arquivo
-    # caso a extensão do arquivo seja diferente de .txt Formato inválido na stderr
-    # A função deve retornar uma lista contendo as linhas do arquivo
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
