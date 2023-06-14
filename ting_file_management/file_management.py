80768ddbf96994bdb17aa88402b4a543c80bbc05
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-14 15:41:08 -0300

    add obs

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -9,3 +9,6 @@ def txt_importer(path_file):
             return file.read().splitlines()
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+# O splitlines()método divide uma string em uma lista.
+# A divisão é feita nas quebras de linha
