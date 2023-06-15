0089befabeb20336d9df09c0998c3a9e66d19d09
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-14 22:24:11 -0300

    task 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    counter = 0
+
+    for element in range(len(instance)):
+        row = instance.search(element)
+        frame = {
+            "palavra": word,
+            "arquivo": row["nome_do_arquivo"],
+            "ocorrencias": [{
+                "linha": index + 1
+            } for index, linha in enumerate(row["linhas_do_arquivo"])
+                if word.lower() in linha.lower()]
+        }
+        result.append(frame)
+        if len(frame["ocorrencias"]) == 0:
+            counter += 1
+
+    if counter == len(instance):
+        return []
+    return result
 
 
 def search_by_word(word, instance):
