896f644a6ab5629af9f3143484c66a122b6b1717
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 17:11:36 -0300

    teste priority_queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -5,21 +5,25 @@ import pytest
 def test_basic_priority_queueing():
     priority_queue = PriorityQueue()
 
-    priority_queue.enqueue({"quantidade_de_linhas": 9})
-    priority_queue.enqueue({"quantidade_de_linhas": 4})
-    priority_queue.enqueue({"quantidade_de_linhas": 2})
-    priority_queue.enqueue({"quantidade_de_linhas": 5})
-    priority_queue.enqueue({"quantidade_de_linhas": 7})
-    priority_queue.enqueue({"quantidade_de_linhas": 11})
-    priority_queue.enqueue({"quantidade_de_linhas": 3})
+    priority_queue.enqueue({"qtd_linhas": 9})
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 2})
+    priority_queue.enqueue({"qtd_linhas": 5})
+    priority_queue.enqueue({"qtd_linhas": 7})
+    priority_queue.enqueue({"qtd_linhas": 11})
+    priority_queue.enqueue({"qtd_linhas": 3})
 
     assert len(priority_queue) == 7
-    assert priority_queue.search(1) == {"quantidade_de_linhas": 6}
+    assert priority_queue.search(1) == {"qtd_linhas": 2}
 
+    delete = priority_queue.high_priority.dequeue()
     assert len(priority_queue) == 6
-    priority_queue.dequeue({"quantidade_de_linhas": 5})
+    assert delete == {"qtd_linhas": 4}
+    
 
+    delete = priority_queue.dequeue()
     assert len(priority_queue) == 5
-    priority_queue.dequeue({"quantidade_de_linhas": 7})
+    assert delete == {"qtd_linhas": 2}
+
     with pytest.raises(Exception):
-        priority_queue.search(7)
+        priority_queue.search(8)
