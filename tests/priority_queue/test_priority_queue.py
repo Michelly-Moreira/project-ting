6d8440075aae626f20a3e3dd50ca8beb0c6f7f1a
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 00:47:26 -0300

    tentanto fazer o teste

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -3,5 +3,20 @@ import pytest
 
 
 def test_basic_priority_queueing():
-    with pytest.raises(TypeError):
-        PriorityQueue()
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue({"quantidade_de_linhas": 9})
+    priority_queue.enqueue({"quantidade_de_linhas": 4})
+    priority_queue.enqueue({"quantidade_de_linhas": 2})
+    priority_queue.enqueue({"quantidade_de_linhas": 5})
+    priority_queue.enqueue({"quantidade_de_linhas": 7})
+    priority_queue.enqueue({"quantidade_de_linhas": 11})
+    priority_queue.enqueue({"quantidade_de_linhas": 3})
+
+    assert len(priority_queue) == 7
+    priority_queue.dequeue({"quantidade_de_linhas": 6})
+
+    assert len(priority_queue) == 6
+    priority_queue.dequeue({"quantidade_de_linhas": 7})
+    with pytest.raises(Exception):
+        priority_queue.search(11)
