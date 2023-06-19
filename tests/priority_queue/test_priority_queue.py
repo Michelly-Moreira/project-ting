d9506b0f7f56d0ef2e8109af9c9567f0586f04c9
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-19 01:01:16 -0300

    adicionando ao teste, search

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -14,9 +14,12 @@ def test_basic_priority_queueing():
     priority_queue.enqueue({"quantidade_de_linhas": 3})
 
     assert len(priority_queue) == 7
-    priority_queue.dequeue({"quantidade_de_linhas": 6})
+    assert priority_queue.search(1) == {"quantidade_de_linhas": 6}
 
     assert len(priority_queue) == 6
+    priority_queue.dequeue({"quantidade_de_linhas": 5})
+
+    assert len(priority_queue) == 5
     priority_queue.dequeue({"quantidade_de_linhas": 7})
     with pytest.raises(Exception):
-        priority_queue.search(11)
+        priority_queue.search(7)
